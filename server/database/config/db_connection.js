f629c98d98054e06a820058ca0e97694185f7b71
Fatma Siam <fatma.o.siam@gmail.com>
2019-07-01 23:52:46 +0300

    edit database url connection

--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -5,7 +5,7 @@ let connectionString = '';
 
 switch (process.env.NODE_ENV) {
   case 'testing': connectionString = process.env.TESTING_DB; break;
-  case 'production': connectionString = process.env.DATABASE_HEROCU; break;
+  case 'production': connectionString = process.env.DATABASE_HRROKU; break;
   case 'development': connectionString = process.env.DATABASE_URL; break;
   default: throw new Error('Database url is not found!!!');
 }
